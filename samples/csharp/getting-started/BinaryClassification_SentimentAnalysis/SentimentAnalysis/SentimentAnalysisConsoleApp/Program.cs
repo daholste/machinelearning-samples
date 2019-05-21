91082024d2d192159faf42dc3edd6e4226a8d92e
Daniel Holstein <daholste@microsoft.com>
2019-05-21 11:15:35 -0700

    Loading models from disk

--- samples/csharp/getting-started/BinaryClassification_SentimentAnalysis/SentimentAnalysis/SentimentAnalysisConsoleApp/Program.cs
+++ samples/csharp/getting-started/BinaryClassification_SentimentAnalysis/SentimentAnalysis/SentimentAnalysisConsoleApp/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.ML;
 using SentimentAnalysisConsoleApp.DataStructures;
 using Common;
 using static Microsoft.ML.DataOperationsCatalog;
+using System.Collections.Generic;
 
 namespace SentimentAnalysisConsoleApp
 {
@@ -25,55 +26,13 @@ namespace SentimentAnalysisConsoleApp
             // Create MLContext to be shared across the model creation workflow objects 
             // Set a random seed for repeatable/deterministic results across multiple trainings.
             var mlContext = new MLContext(seed: 1);
-
-            #region step1to3
-            // STEP 1: Common data loading configuration
-            IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentIssue>(DataPath, hasHeader: true);
-
-            TrainTestData trainTestSplit = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
-            IDataView trainingData = trainTestSplit.TrainSet;
-            IDataView testData = trainTestSplit.TestSet;
-
-            // STEP 2: Common data process configuration with pipeline data transformations          
-            var dataProcessPipeline = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentIssue.Text));
-
-            // STEP 3: Set the training algorithm, then create and config the modelBuilder                            
-            var trainer = mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features");
-            var trainingPipeline = dataProcessPipeline.Append(trainer);
-            #endregion
-
-            #region step4
-            // STEP 4: Train the model fitting to the DataSet
-            ITransformer trainedModel = trainingPipeline.Fit(trainingData);
-            #endregion
-
-            #region step5
-            // STEP 5: Evaluate the model and show accuracy stats
-            var predictions = trainedModel.Transform(testData);
-            var metrics = mlContext.BinaryClassification.Evaluate(data: predictions, labelColumnName: "Label", scoreColumnName: "Score");
-            #endregion
-
-            ConsoleHelper.PrintBinaryClassificationMetrics(trainer.ToString(), metrics);
-
-            // STEP 6: Save/persist the trained model to a .ZIP file
-            mlContext.Model.Save(trainedModel, trainingData.Schema, ModelPath);
-
-            Console.WriteLine("The model is saved to {0}", ModelPath);
-
-            // TRY IT: Make a single test prediction loding the model from .ZIP file
-            SentimentIssue sampleStatement = new SentimentIssue { Text = "This is a very rude movie" };
-
-            #region consume
-            // Create prediction engine related to the loaded trained model
-            var predEngine = mlContext.Model.CreatePredictionEngine<SentimentIssue, SentimentPrediction>(trainedModel);
-
-            // Score
-            var resultprediction = predEngine.Predict(sampleStatement);
-            #endregion
-
-            Console.WriteLine($"=============== Single Prediction  ===============");
-            Console.WriteLine($"Text: {sampleStatement.Text} | Prediction: {(Convert.ToBoolean(resultprediction.Prediction) ? "Toxic" : "Non Toxic")} sentiment | Probability of being toxic: {resultprediction.Probability} ");
-            Console.WriteLine($"================End of Process.Hit any key to exit==================================");
+            
+            var loadedModels = new List<ITransformer>();
+            for (var i = 0; i < 13; i++)
+            {
+                var model = mlContext.Model.Load(@"C:\src\MlDotNetSamples\samples\csharp\getting-started\BinaryClassification_SentimentAnalysis\SentimentAnalysis\MLModels\SentimentModel.zip", out var inputSchema);
+                loadedModels.Add(model);
+            }
             Console.ReadLine();
             #endregion
         }
